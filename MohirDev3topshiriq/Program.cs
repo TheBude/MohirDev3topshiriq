027b18e2ab970f194af6f2371fc299f34f14fc34
TheBude <husaenbobmuratov192@gmail.com>
2024-04-04 22:11:48 +0500

    Vaqt hisoblash

--- MohirDev3topshiriq/Program.cs
+++ MohirDev3topshiriq/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Bu daqiqani soatga aylantiradigan dastur!");
+﻿//1-misol minut=>soat
+Console.WriteLine("Bu daqiqani soatga aylantiradigan dastur!");
+
+Console.Write("Daqiqani Kiriting: ");
+int time = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{time} daqiqa => procressing... Soat");
+
+int hours = time / 60;
+int min = hours * 60;
+int cal = time - min;
+Console.WriteLine($"soatga aylantirilganda: {hours} : {cal}");
