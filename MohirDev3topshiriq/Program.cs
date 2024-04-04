efecea28da8528af20eb9b0529b545bb3b88e606
TheBude <husaenbobmuratov192@gmail.com>
2024-04-04 23:40:16 +0500

    Ballarni o'rta arximetikini hisoblash uichun

--- MohirDev3topshiriq/Program.cs
+++ MohirDev3topshiriq/Program.cs
@@ -1,23 +1,27 @@
-﻿//2-misol yoshni toifalash
-Console.Write("Salom!\nIsmingizni kiriting =>  ");
-string name = Console.ReadLine();
+﻿Console.WriteLine("Men fanlardan olgan balingizni toifalayman!\nFanlardan olgan balingizni 0-100 gacha kirting.");
 
-Console.Write($"{name} bu dastur yoshingizni toifalaydi\nYoshingizni Kirting => ");
-int age = Convert.ToInt32( Console.ReadLine() );
+Console.Write("Matimatika Balingiz => ");
+int score = Convert.ToInt32(Console.ReadLine());
 
-if (age >= 0 && age <= 12)
-{
-    Console.WriteLine($"{name} Sizning yoshingiz <bola> lar toifasiga to'g'ri keldi ");
-}
-else if (age >= 13 && age <= 19)
-{
-    Console.WriteLine($"{name} Sizning yoshingiz <o'smir> lar toifasiga to'g'ri keldi");
-}
-else if (age >= 20 && age <= 59)
-{
-    Console.WriteLine($"{name} Sizning yoshingiz <katta> lar toifasiga to'g'ri keldi");
-}
-else
-{
-    Console.WriteLine($"{name} Sizning yoshingiz <qariya> lar toifasiga to'g'ri keldi");
-}
+Console.Write("Tarix Balingiz => ");
+int score1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Biologiya Balingiz => ");
+int score2 = Convert.ToInt32(Console.ReadLine());
+
+int add = score + score1 + score2;
+int middleArchimetic = add / 3;
+
+Console.WriteLine($"Sizning o'rtacha 3ta fandan ballingiz: {middleArchimetic}");
+
+string work =
+    middleArchimetic >= 80 && middleArchimetic <= 100
+        ? "Siz A'lo darajada bal olgansiz"
+        : middleArchimetic >= 60 && middleArchimetic <= 79
+            ? "Siz Yaxshi darajada bal olgansiz"
+            : middleArchimetic >= 40 && middleArchimetic <= 59
+                ? "Siz Qoniqarli darajada bal olgansiz"
+                : middleArchimetic <= 40
+                    ? "Siz Qoniqarsiz darajada bal olgansiz"
+                    : "not fount";
+Console.WriteLine(work);
